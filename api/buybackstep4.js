9cb9e858e8a541702d7adba401c272a0decc3092
eacollectibles <ea.collectibles.service@gmail.com>
2025-06-12 14:01:26 -0700

    Add files via upload
    added new things

--- api/buybackstep4.js
+++ api/buybackstep4.js
@@ -1,5 +1,5 @@
 
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
